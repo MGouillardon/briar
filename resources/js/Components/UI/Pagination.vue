ec967b0c864d78017ded22f75ec1af303fb08750
Maxime <maxime.gouillardon@gmail.com>
2023-09-06 09:03:36 +0200

    fix: es-lint error using v-html on pagination component

--- resources/js/Components/UI/Pagination.vue
+++ resources/js/Components/UI/Pagination.vue
@@ -1,18 +1,22 @@
 <script setup>
-import {Link} from '@inertiajs/vue3'
+import { Link } from "@inertiajs/vue3";
 
-defineProps({links:Array})
-
+defineProps({ links: Array });
 </script>
 
 <template>
-  <div class="flex gap-1 absolute bottom-8">
-    <Link
-      v-for="(link, index) in links"
-      :key="index" class="py-2 px-4 rounded-md"
-      :href="link.url"
-      :class="{'bg-indigo-500 text-white dark:bg-indigo-700 dark:text-gray-300': link.active}"
-      v-html="link.label"
-    >
-  </div>
+    <div class="flex gap-1 absolute bottom-8">
+        <Link
+            v-for="(link, index) in links"
+            :key="index"
+            class="py-2 px-4 rounded-md"
+            :href="link.url"
+            :class="{
+                'bg-indigo-500 text-white dark:bg-indigo-700 dark:text-gray-300':
+                    link.active,
+            }"
+        >
+            <div v-html="link.label"></div>
+        </Link>
+    </div>
 </template>

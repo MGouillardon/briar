e73d5587ed9aabbca7a39eadc21e8033f5ab6185
Maxime <maxime.gouillardon@gmail.com>
2023-09-18 07:48:56 +0200

    feat: add views umploading images

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -13,8 +13,9 @@ const flash = computed(() => page.props.flash.success)
     <main class="container mx-auto p-4 w-full">
         <div
       v-if="flash"
-      class="border rounded-md shadow-sm text-center border-green-200 dark:border-green-800 bg-green-500 dark:bg-green-900 p-2"
+      class="mb-4 border rounded-md shadow-sm text-center border-green-200 dark:border-green-800 bg-green-500 dark:bg-green-900 p-2"
     >
+    
       {{ flash }}
     </div>
         <slot>Default</slot>
--- resources/js/Pages/Realtor/PropertyImage/Create.vue
+++ resources/js/Pages/Realtor/PropertyImage/Create.vue
@@ -1,15 +1,35 @@
 <script setup>
-defineProps({
-    property: {
-        type: Object,
-        required: true
-    }
+import Box from "@/Components/UI/Box.vue";
+import { useForm } from "@inertiajs/vue3";
+const props = defineProps({ property: Object });
+const form = useForm({
+    images: [],
 });
+const upload = () => {
+    form.post(
+        route("realtor.property.image.store", { property: props.property.id }),
+        {
+            onSuccess: () => form.reset(),
+        }
+    );
+};
+const addFiles = (event) => {
+    for (const image of event.target.files) {
+        form.images.push(image);
+    }
+};
+const reset = () => form.reset();
 </script>
 
 <template>
-    <form enctype="multipart/form-data" method="post" :action="route('realtor.property.image.store', {property: property.id})">
-        <input type="file" multiple name="images[]">
-        <button type="submit">Upload</button>
-    </form>
+    <Box>
+        <template #header> Upload new Images </template>
+        <form @submit.prevent="upload">
+            <input type="file" multiple @input="addFiles" />
+            <button class="btn-outline" type="submit">Upload</button>
+            <button class="btn-outline" type="reset" @click="reset">
+                Reset
+            </button>
+        </form>
+    </Box>
 </template>

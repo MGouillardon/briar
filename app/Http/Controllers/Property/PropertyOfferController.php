5434878d8b3fe6e54cfe14e59e1a3bb1a2d62fc8
Maxime <maxime.gouillardon@gmail.com>
2023-09-24 10:14:52 +0200

    feat: send notifications and store in DB

--- app/Http/Controllers/Property/PropertyOfferController.php
+++ app/Http/Controllers/Property/PropertyOfferController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers\Property;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\PropertyOfferRequest;
 use App\Models\Offer;
 use App\Models\Property;
 use Illuminate\Http\Request;
+use App\Notifications\OfferMade;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PropertyOfferRequest;
 
 class PropertyOfferController extends Controller
 {
     public function store(Property $property, PropertyOfferRequest $request)
     {
         $this->authorize('view', $property);
-        $property->offers()->save(
+        $offer = $property->offers()->save(
             Offer::make(
                 $request->validated()
             )
@@ -21,6 +22,10 @@ class PropertyOfferController extends Controller
                     $request->user()
                 )
         );
+        $property->owner->notify(
+            new OfferMade($offer)
+        );
+
         return redirect()->back()->with('success', 'Offer sent successfully');
     }
 }
